960054ac67fb7c0f48982db008b1d0bbf4f21621
Giulia Barbizan <giuliabarbizan@gmail.com>
2025-03-21 20:51:10 -0300

    criando estrutura do projeto

---
+++ src/Home.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+        <section className=''>
+            <img src="" alt="Giulia Barbizan" />
+            <a href="">Linkedin</a>
+            <a href="">Github</a>
+            <h2>Olá, eu sou a Giulia Barbizan!</h2>
+        </section>
+        <section id='Sobre'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam natus quisquam architecto officia accusamus optio sequi nisi distinctio eveniet aut dignissimos illo at pariatur, nemo quos alias explicabo doloribus. Necessitatibus repudiandae officia doloremque deleniti vel qui ad cum nostrum architecto, error dignissimos? Dignissimos sunt, iusto sed voluptates error eius distinctio?</p>
+            <a href="">Download CV</a>
+            
+        </section>
+        <section id='Projetos'>
+            <div>card 1</div>
+            <div>card 2</div>
+            <div>card 3</div>
+            <div>card 4</div>
+        </section>
+        <section id='skills'>
+            <div><img src="" alt="" /></div>
+            <div><img src="" alt="" /></div>
+            <div><img src="" alt="" /></div>
+            <div><img src="" alt="" /></div>
+            <div><img src="" alt="" /></div>
+            <div><img src="" alt="" /></div>
+            <div><img src="" alt="" /></div>
+            <div><img src="" alt="" /></div>
+        </section>
+        <section id='contato'>
+
+        </section>
+    </>
+  )
+}
+
+export default Home
---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav>
+        <ul>
+            <li><a href="">Sobre mim</a></li>
+            <li><a href="">Projetos</a></li>
+            <li><a href="">Habilidades</a></li>
+            <li><a href="">Contato</a></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Nav
